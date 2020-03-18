ffec24a9ff4c23dc350d1da3b6e82968858d8be9
Lizzy-j <lizzyhj@hotmail.com>
2020-03-18 16:16:02 +0000

    Create tests to check if add tasks function adds something Relates #1 Co-authored by Roger <fac@technicalbloke.com>

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 console.log("Running main app javascript.");
 console.log("Done.");
+
+const toDoList = document.getElementById("toDoList");
+
+function addTask(priority, text, status){
+    let elem = document.createElement("li");
+    elem.innerHTML = "A LIST ITEM!!!";   
+    toDoList.appendChild(elem);
+}
--- tests.js
+++ tests.js
@@ -1,8 +1,16 @@
 console.log("Running tests...");
 console.log("Done.");
 
+test("Check the function exists", t => {
+    addTask("low","Clean the fridge",false);
+});
+
 test("Submitting a new task adds it to the list", t => {
-    // test goes here
+    addTask("low","Clean the fridge",false);
+    //let toDoList = document.getElementById("toDoList")
+    let toDoListLength = document.querySelectorAll(".to-do-list > li").length
+    console.log(toDoListLength)
+    t.equal (toDoListLength > 0, true)
 });
 
 test("Checking an entry marks it as complete", t => {
