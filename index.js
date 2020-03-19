006934b046fc0b71e0398f61a29e31dd0341ea61
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-03-19 16:38:15 +0000

    adding test case for checkbox

--- index.js
+++ index.js
@@ -3,65 +3,69 @@ console.log("Done.");
 
 const toDoList = document.getElementById("toDoList");
 const doneList = document.getElementById("doneList");
-const toDoTab = document.getElementById("tabsToDo")
+const toDoTab = document.getElementById("tabsToDo");
 const doneTab = document.getElementById("tabsDone");
 
 doneTab.addEventListener("click", () => {
-    toDoList.classList.add("hidden");
-    doneList.classList.remove("hidden")
+  toDoList.classList.add("hidden");
+  doneList.classList.remove("hidden");
 });
 toDoTab.addEventListener("click", () => {
   doneList.classList.add("hidden");
-  toDoList.classList.remove("hidden")
+  toDoList.classList.remove("hidden");
 });
-
 
 const lowPriority = document.getElementById("priorityLow");
 const mediumPriority = document.getElementById("priorityMedium");
 const highPriority = document.getElementById("priorityHigh");
 
-const formPriorityRadios = document.querySelectorAll('input[name=priority]');
-const form = document.querySelector('.new-task-form');
+const formPriorityRadios = document.querySelectorAll("input[name=priority]");
+const form = document.querySelector(".new-task-form");
 const formText = document.getElementById("newTaskFormText");
 
-const formSubmit = document.getElementById("formButton")
+const formSubmit = document.getElementById("formButton");
 
-function addTask(priority, text, checkBox){
-    
-    // This function clones the card template and inserts it into the
-    // task list using the parameters supplied.
+function addTask(priority, text, checkBox) {
+  // This function clones the card template and inserts it into the
+  // task list using the parameters supplied.
 
-    let template = document.getElementById('cardTemplate');
-    let newTask = template.content.cloneNode(true)
+  let template = document.getElementById("cardTemplate");
+  let newTask = template.content.cloneNode(true);
 
-    let thePriority = newTask.querySelector(".to-do-list__card__priority");
-    thePriority.classList.add(priority);
-    
-    let theText = newTask.querySelector(".to-do-list__card__text");
-    theText.textContent = text;
-    
-    let theStatus = newTask.querySelector(".to-do-list__card__checkbox");
+  let thePriority = newTask.querySelector(".to-do-list__card__priority");
+  thePriority.classList.add(priority);
 
-    theStatus.checked = checkBox;
+  let theText = newTask.querySelector(".to-do-list__card__text");
+  theText.textContent = text;
 
-    theStatus.addEventListener("click", () => {
-        if(theStatus.checked) {
-            doneList.appendChild(newTask);
-            toDoList.removeChild(newTask);
-        }
-    })
+  let theStatus = newTask.querySelector(".to-do-list__card__checkbox");
 
-    toDoList.appendChild(newTask);
+  theStatus.checked = checkBox;
+
+  toDoList.appendChild(newTask);
+  console.log(newTask)
+  theStatus.addEventListener("click", () => {
+    if (theStatus.checked) {
+        console.log(newTask)
+        toDoList.removeChild(newTask);
+        doneList.appendChild(newTask);
+    }
+    console.log("I am clicked" + priority)
+  });
+
 }
 
-formSubmit.addEventListener("click", event => {
-    event.preventDefault();
-    let selected = [...formPriorityRadios].filter( el => el.checked )[0];
-    addTask(selected.value, formText.textContent, false);
+formSubmit.addEventListener(heStatus.checked) {
+    console.log(newTask)
+    toDoList.removeChild(newTask);
+    doneList.appendChild(newTask);
+}"click", event => {
+  event.preventDefault();
+  let selected = [...formPriorityRadios].filter(el => el.checked)[0];
+  addTask(selected.value, formText.textContent, false);
+});
 
-})
-
-function addTaskToDone(){
+function addTaskToDone() {
   // If status checked is true
   // Add class done to task
   // Populate done list with done tasks
--- tests.js
+++ tests.js
@@ -1,83 +1,87 @@
-function clearList(listSelector){
-    let theList = document.querySelectorAll(listSelector);
-    theList.forEach( elem =>{
-        elem.remove();
-        }
-    );
+function clearList(listSelector) {
+  let theList = document.querySelectorAll(listSelector);
+  theList.forEach(elem => {
+    elem.remove();
+  });
 }
 
-
 test("Check the function exists", t => {
-    addTask("low","Clean the fridge",false);
+  addTask("low", "Clean the fridge", false);
 });
 
 test("Submitting a new task adds something to the list", t => {
-    addTask("low","Clean the fridge",false);
-    //let toDoList = document.getElementById("toDoList")
-    let toDoListLength = document.querySelectorAll(".to-do-list > li").length
-    console.log(toDoListLength)
-    t.equal (toDoListLength > 0, true)
-    clearList(".to-do-list > li");
+  addTask("low", "Clean the fridge", false);
+  //let toDoList = document.getElementById("toDoList")
+  let toDoListLength = document.querySelectorAll(".to-do-list > li").length;
+  console.log(toDoListLength);
+  t.equal(toDoListLength > 0, true);
+  clearList(".to-do-list > li");
 });
 
 test("Submitting a new task adds the correct template to the list", t => {
-    addTask("low","Clean the fridge",false);
-    let thePriority = document.querySelector(".to-do-list > li:last-child > div") !== null;
-    let theText = document.querySelector(".to-do-list > li:last-child > p") !== null;
-    let theStatus = document.querySelector(".to-do-list > li:last-child > input") !== null;
-    t.equal (thePriority && theText && theStatus, true);
-    clearList(".to-do-list > li");
+  addTask("low", "Clean the fridge", false);
+  let thePriority =
+    document.querySelector(".to-do-list > li:last-child > div") !== null;
+  let theText =
+    document.querySelector(".to-do-list > li:last-child > p") !== null;
+  let theStatus =
+    document.querySelector(".to-do-list > li:last-child > input") !== null;
+  t.equal(thePriority && theText && theStatus, true);
+  clearList(".to-do-list > li");
 });
 
 test("Checking the correct data is added", t => {
-    addTask("low","Clean the fridge",false);
-    let thePriority = document.querySelector(".to-do-list > li:last-child > div");
-    let theText = document.querySelector(".to-do-list > li:last-child > p");
-    let theStatus = document.querySelector(".to-do-list > li:last-child > input");
-    t.equal(thePriority.classList.contains("low"), true);
-    t.equal(theText.textContent, "Clean the fridge");
-    t.equal(theStatus.checked, false);
-    clearList(".to-do-list > li");
+  addTask("low", "Clean the fridge", false);
+  let thePriority = document.querySelector(".to-do-list > li:last-child > div");
+  let theText = document.querySelector(".to-do-list > li:last-child > p");
+  let theStatus = document.querySelector(".to-do-list > li:last-child > input");
+  t.equal(thePriority.classList.contains("low"), true);
+  t.equal(theText.textContent, "Clean the fridge");
+  t.equal(theStatus.checked, false);
+  clearList(".to-do-list > li");
 });
 
 test("Checking if submiting the new task form addes a task to the to-do list", t => {
-    // addTask("low","Clean the fridge",false);
-    highPriority.click();
-    formText.textContent = "submit integration test.";
-    formSubmit.click();
-    
-    let thePriority = document.querySelector(".to-do-list > li:last-child > div");
-    let theText = document.querySelector(".to-do-list > li:last-child > p");
-    let theStatus = document.querySelector(".to-do-list > li:last-child > input");
-    t.equal(thePriority.classList.contains("high"), true);
-    t.equal(theText.textContent, "submit integration test.");
-    t.equal(theStatus.checked, false);
-    clearList(".to-do-list > li");
+  // addTask("low","Clean the fridge",false);
+  highPriority.click();
+  formText.textContent = "submit integration test.";
+  formSubmit.click();
+
+  let thePriority = document.querySelector(".to-do-list > li:last-child > div");
+  let theText = document.querySelector(".to-do-list > li:last-child > p");
+  let theStatus = document.querySelector(".to-do-list > li:last-child > input");
+  t.equal(thePriority.classList.contains("high"), true);
+  t.equal(theText.textContent, "submit integration test.");
+  t.equal(theStatus.checked, false);
+  clearList(".to-do-list > li");
 });
 
 test("Checking an entry marks it as complete", t => {
   // test goes here
+  addTask("low", "Clean the fridge", false);
+  const checkBoxTest = document.querySelector(".to-do-list__card__checkbox")
+//   checkBoxTest.click()
 });
 
 test("Deleting an entry removes it from the list", t => {
-    // test goes here
+  // test goes here
 });
 
 test("Toggling the filter hides completed tasks from the list", t => {
-    doneTab.click();
-    const result = doneTab.classList.contains("hidden")
-    t.equal(result, false)
-    toDoTab.click()
+  doneTab.click();
+  const result = doneTab.classList.contains("hidden");
+  t.equal(result, false);
+  toDoTab.click();
 });
 
 test("Selecting low radio button returns a correct value when clicked or selected on keyboard", t => {
   const result = lowPriority.value;
   lowPriority.click();
-  t.equal(result, "low");    
+  t.equal(result, "low");
 });
 
 test("Selecting high radio button returns a correct value when clicked or selected on keyboard", t => {
-    const result = highPriority.value;
-    highPriority.click();
-    t.equal(result, "high");    
+  const result = highPriority.value;
+  highPriority.click();
+  t.equal(result, "high");
 });
